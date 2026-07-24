1bb70de7670ab5e0229274b3e406f19d8fb6e409
Hemanth Vundavilli <s2rzh264vf@privaterelay.appleid.com>
2026-07-24 08:20:30 -0400

    Update content.js

--- content.js
+++ content.js
@@ -305,7 +305,7 @@ window.SITE = {
       name: "Hemanth Vundavilli, Ph.D.",
       role: "Assistant Professor of Coastal Oceanography and Systems Science",
       dept: "Department of Marine Science, Coastal Carolina University",
-      education: "Ph.D. Earth Sciences, University of Waikato, New Zealand (2023) · B.Tech + M.Tech Naval Architecture and Ocean Engineering, IIT Madras (2016)",
+      education: "Ph.D. Earth Sciences, University of Waikato, New Zealand (2023)<br>B.Tech + M.Tech Naval Architecture and Ocean Engineering, IIT Madras (2016)",
       //topic: "Coastal morphodynamics, river deltas, sediment transport, Delft3D modeling",
       links: [
         { label: "Google Scholar", href: "http://scholar.google.com/citations?user=FMsnJtsAAAAJ&hl=en&oi=ao" },
